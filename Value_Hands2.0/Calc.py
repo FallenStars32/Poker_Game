01b3e19f4de6228b9a688c152b4d03be261b0909
RougeAce <127271451+RougeAce@users.noreply.github.com>
2023-07-09 21:41:41 -0500

    Added ODDS

--- Value_Hands2.0/Calc.py
+++ Value_Hands2.0/Calc.py
@@ -24,11 +24,11 @@ def possible_hands(hand):
     pos_hands = []
       
     #This loops through the deck and accounts for len_hand which ads itself  
-    for i in range(len(deck)-len_han): #EXP 52 - 5 = 47
+    for i in range((len(deck) - 1)-len_han): #EXP 52 - 5 = 47
         cur_hand = []
         for h in hand:
             cur_hand.append(h)  
-        for j in range(emppty): #7
+        for j in range(emppty - 1): #7
             cur_hand.append(deck[i + j])
             
         pos_hands.append(cur_hand)
@@ -52,8 +52,7 @@ def odds(hand):
         if POK.two_pair(i) == True:
             c += 1
         if POK.three_kind(i) == True:
-            d += 1
-            
+            d += 1 
         if SOF.straight(i) == True:
             e += 1
         if SOF.flush(i) == True:
@@ -66,10 +65,29 @@ def odds(hand):
             x += 1
         if SOF.royal_flush(i) == True:
             j += 1
-      
-          
             
-    return [a / b, c / b, d / b, e/ b, f / b, g / b, h / b, x / b, j / b]
+      
+    pair_odds = a /b 
+    two_pair_odds = c / b
+    three_kind_odds = d / b
+    straight_odds = e / b
+    flush_odds = f / b
+    full_house_odds = g / b
+    four_kind_odds = h / b
+    striaght_flush_odds = x / b
+    royal_flush_odds = j / b
+       
+    pair_odds = pair_odds + three_kind_odds + four_kind_odds
+    three_kind_odds = three_kind_odds + four_kind_odds
+    
+    return [pair_odds,two_pair_odds, three_kind_odds, straight_odds, flush_odds, full_house_odds, four_kind_odds, striaght_flush_odds, royal_flush_odds]
+
+def odds_losing(middle, hand):
+    hands = odds(hand)
+    op_odds = odds(middle)
+    
+    for i, j in hands, op_odds:
+        return 0
                   
         
         
@@ -91,17 +109,5 @@ Color
 
 """
 
-card1 = objects.Cards(1, 1, 1)
-#card2 = objects.Cards(1, 2, 2)
-#card3 = objects.Cards(1, 3, 3)
-#card4 = objects.Cards(1, 4, 4)
-#card5 = objects.Cards(1, 5, 5)
-
-#card6 = objects.Cards(1, 5, 5)
-#card7 = objects.Cards(1, 5, 5)
-
-hand = [card1, card2]
-
-print(odds(hand))
 
 
